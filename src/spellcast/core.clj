a326133bd5bd6666662d0b7542bdbcc624a435bf
Sebastian Panknin <doorn@web.de>
2014-03-09 23:23:28 +0100

    get the controler part in shape

--- src/spellcast/core.clj
+++ src/spellcast/core.clj
@@ -1,12 +1,21 @@
 (ns spellcast.core
+  (:use clojure.tools.logging)
   (:require [clojure.core.async :as async :refer :all]))
 
-(def universe (atom {}))
+(def universe
+  "The central data for all worlds."
+  (atom {}))
 
-(defn init! []
+(defn init!
+  "We start with an empty universe, use this in repl to restart."
+  []
+  (info "Reset universe to initial state")
   (reset! universe {}))
 
-(defn create-world! [name]
+(defn create-world!
+  "Add a new world to the universe."
+  [name]
+  (info (str "world " name " created"))
   (swap! universe
          (fn create-world [worlds]
            (assoc worlds
@@ -14,7 +23,17 @@
                    :started false,
                    :chan (chan)}))))
 
-(defn world [name]
+(defn remove-world!
+  "removes the given world from the univers"
+  [world-name]
+  (info (str "world " world-name " removed."))
+  (swap! universe
+         (fn remove-world [worlds]
+           (dissoc worlds world-name))))
+
+(defn world
+  "Get a specific world from the universe."
+  [name]
   (get @universe name))
 
 (defn update-world!
@@ -30,6 +49,7 @@
 
 (defn add-player
   [world name]
+  (info (str "player " name " world added"))
   (assoc world :players
          (assoc (:players world)
            name {:ready false,
@@ -54,10 +74,10 @@
 
 (defn game-started!!
   "waits until the game started."
-  [world]
+  [world-name]
   (while (and
           (<!! (:chan (world world-name)))
-          (not (:started (world world-name))))
+          (not (:started (world world-name))))))
 
 (defn all-players-ready!!
   "Read from the world channels until all players are ready."
@@ -74,11 +94,21 @@
   (update-world! world-name evaluate-spells)
   (publish-result (world world-name)))
 
+(defn game-over  ;; FIXME
+  "Returns true if the game for the given world is finished."
+  [world]
+  true)
+
 (defn game-loop!!
   "Main game loop runs until the game is over."
   [world-name]
-  (game-started!! (world world-name))
-  (letfn [(game-over [] true)] ;; FIXME
-    (while (not (game-over))
-      (play-round!! world-name))))
+  (game-started!! world-name)
+  (while (not (game-over (world world-name)))
+    (play-round!! world-name))
+  (remove-world! world-name))
 
+(defn new-game!
+  "Start the game in a new world"
+  [world-name]
+  (create-world! world-name)
+  (future (game-loop!! world-name)))
--- src/spellcast/gestures.clj
+++ src/spellcast/gestures.clj
@@ -30,17 +30,27 @@
    (replace-double-gesture gestures)))
 
 (defn match-gesture [spell-guesture hand-guestures side]
+  "A hand guesture matches a spell-gesture if it is a double gesture
+   or a single gesture on the given side."
   (or
    (= spell-guesture hand-guestures)
    (= spell-guesture (side hand-guestures))))
 
 (defn match-spell [spell gestures side]
+  "A spell is found in the (hand) gesture for a given side,
+   if all gesture for the spell are found."
   (every? identity
           (map (fn check-guesture [spell-gesture hand-gestures]
                  (match-gesture spell-gesture hand-gestures side))
                spell gestures)))
 
 (defn find-spells [spell-book gestures side]
+  "Find all the spells in the spell-book,
+   that match the given gestures for the given side.
+   The gestures from the hand, have the most recent action
+   as first element. In spellbock the gestures start with the oldest
+   action and end with the last gesture to make. So the spells have
+   to be reverted."
   (filter (fn check-spell [[spell-gestures _]]
             (match-spell (reverse spell-gestures)
                          gestures
